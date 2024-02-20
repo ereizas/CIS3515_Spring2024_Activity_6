2fa2c9a3dd98ed47affce9e363c6d0aa0b4d08ca
Eric Reizas <tuo17224@temple.edu>
2024-02-20 11:59:22 -0500

    Extracted transferred value to change text size

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -11,6 +11,7 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
+            this.textSize=intent.getIntExtra("Text Sizes",22).toString().toFloat()
 
         }
 
